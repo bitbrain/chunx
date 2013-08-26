85ffabfe0d7e4a43438de44640b410707467339e
Miguel Gonzalez <miguel-gonzalez@gmx.de>
2013-08-27 00:15:36 +0200

    Renamed ChunkInterpreter to PositionInterpreter

--- src/de/myreality/chunx/util/PositionInterpreter.java
+++ src/de/myreality/chunx/util/PositionInterpreter.java
@@ -25,7 +25,7 @@ package de.myreality.chunx.util;
  * @since 1.0
  * @version 1.0
  */
-public interface ChunkInterpreter {
+public interface PositionInterpreter {
 
 	// ===========================================================
 	// Constants
