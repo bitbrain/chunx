02999786bb71d94b1f67e8be764a231110524b19
Miguel Gonzalez <miguel-gonzalez@gmx.de>
2013-08-29 13:35:49 +0200

    Added test for SimpleChunkFactory

---
+++ src/de/myreality/chunx/ChunkFactory.java
@@ -0,0 +1,46 @@
+/*
+ * chunx is a Java 2D chunk engine to generate "infinite" worlds.
+ * Copyright (C) 2013  Miguel Gonzalez
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package de.myreality.chunx;
+
+/**
+ * Factory to create new chunk objects
+ * 
+ * @author Miguel Gonzalez <miguel-gonzalez@gmx.de>
+ * @since 1.0
+ * @version 1.0
+ */
+public interface ChunkFactory {
+
+	// ===========================================================
+	// Constants
+	// ===========================================================
+
+	// ===========================================================
+	// Methods
+	// ===========================================================
+	
+	/**
+	 * Creates a new chunk at the given index
+	 * 
+	 * @param indexX x index
+	 * @param indexY y index
+	 * @return new chunk instance
+	 */
+	Chunk createChunk(int indexX, int indexY, ChunkConfiguration configuration);
+}
---
+++ test/de/myreality/chunx/SimpleChunkFactoryTest.java
@@ -0,0 +1,70 @@
+/*
+ * chunx is a Java 2D chunk engine to generate "infinite" worlds.
+ * Copyright (C) 2013  Miguel Gonzalez
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package de.myreality.chunx;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Test case for {@link SimpleChunkFactory}
+ * 
+ * @author Miguel Gonzalez <miguel-gonzalez@gmx.de>
+ * @since 1.0
+ * @version 1.0
+ */
+public class SimpleChunkFactoryTest {
+
+	// ===========================================================
+	// Definitions
+	// ===========================================================
+	
+	ChunkFactory factory;
+
+	// ===========================================================
+	// Setup
+	// ===========================================================
+
+	@Before
+	public void setUp() throws Exception {
+		factory = new SimpleChunkFactory();
+	}
+
+	// ===========================================================
+	// Test cases
+	// ===========================================================
+	
+	@Test
+	public void testCreateChunk() {
+		
+		final int INDEX_X = 10, INDEX_Y = 82;
+		
+		ChunkConfiguration configuration = new SimpleChunkConfiguration();
+		
+		Chunk chunk = factory.createChunk(INDEX_X, INDEX_Y, configuration);
+		
+		assertTrue("Chunk should not be null", chunk != null);	
+		assertTrue("Chunk x index doesn't match.", chunk.getIndexX() == INDEX_X);	
+		assertTrue("Chunk y index doesn't match.", chunk.getIndexY() == INDEX_Y);	
+		assertTrue("Chunk x position doesn't match.", chunk.getX() == INDEX_X * configuration.getChunkWidth());
+		assertTrue("Chunk y position doesn't match.", chunk.getY() == INDEX_Y * configuration.getChunkHeight());
+	}
+
+}
